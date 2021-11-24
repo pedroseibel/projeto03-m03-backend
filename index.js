62566d615113f2b5a90ec8250518304598f47ef5
pedroseibel <pedrosbl@icloud.com>
2021-11-24 17:00:43 -0300

    Imported cors on index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();  
 require('dotenv').config(); 
 app.use(express.json());  
+const cors = require("cors");
 const port = process.env.PORT || 3000;
 
 const conn = require("./model/conn/index"); 
